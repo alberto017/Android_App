7c70c6c464645977b0c482b97befc5e5549edbe3
LuisEliasTec <luiseliastec@hotmail.com>
2016-05-11 00:47:55 -0600

    Commit 3 - AGENDA_PRESTAMOS
    Tercer commit
    Actualización:
    Inicio Splash
    Pestañas (basico)

--- AGENDA_PRESTAMOS/app/src/main/java/com/example/elias/agenda_prestamos/MainActivity.java
+++ AGENDA_PRESTAMOS/app/src/main/java/com/example/elias/agenda_prestamos/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.elias.agenda_prestamos;
 
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +12,55 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        /**INDICAR TITULO Y SUBTITULO**/
+        actionBar.setTitle("Agenda Prestamos");
+        actionBar.setSubtitle("Administra tus prestamos de forma adecuada");
+
+        /**MODO TABS EN ACTIONBAR**/
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        /**CREAR TABS**/
+        ActionBar.Tab tab = actionBar.newTab()
+                .setText("Registrar")
+                .setTabListener(new TabsListener(
+                        this, "Registrar Préstamo", Pestaña_Registro.class));
+        actionBar.addTab(tab);
+
+        tab = actionBar.newTab()
+                .setText("Pendientes")
+                .setTabListener(new TabsListener(
+                        this, "Consultar Préstamos Pendientes", Pestaña_Consulta_Pendientes.class));
+        actionBar.addTab(tab);
+
+        tab = actionBar.newTab()
+                .setText("Devueltos")
+                .setTabListener(new TabsListener(
+                        this, "Consultar Préstamos Devueltos", Pestaña_Consulta_Devueltos.class));
+        actionBar.addTab(tab);
+    }
+
+    public class TabsListener  implements ActionBar.TabListener {
+
+        private Fragment fragment;
+        private final String tag;
+
+        public TabsListener(Activity activity, String tag, Class cls) {
+            this.tag = tag;
+            fragment = Fragment.instantiate(activity, cls.getName());
+        }
+
+        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+            ft.replace(android.R.id.content, fragment, tag);
+        }
+
+        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+            ft.remove(fragment);
+        }
+
+        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {}
     }
 }
---
+++ AGENDA_PRESTAMOS/app/src/main/java/com/example/elias/agenda_prestamos/Pestaña_Registro.java
@@ -0,0 +1,21 @@
+package com.example.elias.agenda_prestamos;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Elias on 10/05/16.
+ */
+public class Pestaña_Registro extends Fragment {
+
+    View rootView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fm_registro, container, false);
+        return rootView;
+    }
+}
